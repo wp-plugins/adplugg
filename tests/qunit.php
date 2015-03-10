c400751f290ac5dedf5161e4836ad86826968860
adplugg <adplugg@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-03-10 14:04:53 +0000

    Changed QUnit naming conventions, added QUnit testing to the admin options page.
    git-svn-id: https://plugins.svn.wordpress.org/adplugg/trunk@1109474 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- tests/qunit.php
+++ tests/qunit.php
@@ -5,14 +5,18 @@
  * @since 1.1.16
  */
 
-function load_qunit() {
+/**
+ * Loads QUnit and the tests for the passed test suite.
+ * @param string $test_suite The test suite that you want to load.
+ */
+function load_qunit($test_suite) {
     ?>
         <link rel="stylesheet" href="//code.jquery.com/qunit/qunit-1.15.0.css">
         <div id="qunit"></div>
         <div id="qunit-fixture"></div>
         <script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js" type="text/javascript"></script>
         <script src="//code.jquery.com/qunit/qunit-1.15.0.js"></script>
-        <script src="<?php echo plugins_url('qunit-tests.js', __FILE__); ?>"></script>
+        <script src="<?php echo plugins_url('qunit-test-' . $test_suite . '.js', __FILE__); ?>"></script>
     <?php
 }
 
